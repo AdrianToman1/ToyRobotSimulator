145952485a5000aa19550b9d2a6b55bdd318a0c5
AdrianToman1 <adrian@adriantoman.com>
2020-09-01 09:04:17 +1000

    Refactor console app into a seperate project

---
+++ ToyRobotSimulator.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToyRobotSimulator.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
